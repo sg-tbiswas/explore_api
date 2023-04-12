34c671841a2eeb7c99d1ef6370c1cb46053004d5
Tamojit <tamojit@sentientgeeks.com>
2023-04-12 17:44:19 +0530

    db uri change

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-  // DB_CONNECTION_URI: "mongodb://goby-admin:rtfgcgdxrkjbh@54.166.96.55:27017",
-  DB_CONNECTION_URI: "mongodb://localhost:27017?retryWrites=true&w=majority",
+  DB_CONNECTION_URI: "mongodb://gobyroot:hefrt%23dh53Tuj@54.166.96.55:27017",
+  // DB_CONNECTION_URI: "mongodb://localhost:27017?retryWrites=true&w=majority",
   DB_NAME: "gobyHomes",
 });
