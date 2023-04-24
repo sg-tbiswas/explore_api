6c006603ecfd68e463b6644e3a95ce0a6614ed53
Tamojit <tamojit@sentientgeeks.com>
2023-04-24 11:43:55 +0530

    cron package update

--- cron.js
+++ cron.js
@@ -1,35 +1,55 @@
 const Cron = require("croner");
+const nodecron = require("node-cron");
 const gobyHomes = require("./insertion");
 const recordUpdate = require("./updation");
 const imageUpload = require("./imageUpload");
 const concatePropertyImages = require("./concatePropertyImages");
 const updateBindPropertyImages = require("./updateBindPropertyImages");
 
-Cron("*/30 * * * *", async () => {
-  let fromInsertImage = false;
-  let fromInsertData = false;
-  try {
-    fromInsertImage = await imageUpload();
-    if (fromInsertImage === true) {
-      fromInsertData = await gobyHomes();
-    }
-    // if (fromInsertData === true && fromInsertImage === true) {
-    //   await concatePropertyImages();
-    // }
-    console.log("running a task every 30 minute.");
-  } catch (error) {
-    console.log("Something went wrong in 30 min cron.");
-  }
-});
+// Cron("*/30 * * * *", async () => {
+//   let fromInsertImage = false;
+//   let fromInsertData = false;
+//   try {
+//     fromInsertImage = await imageUpload();
+//     if (fromInsertImage === true) {
+//       fromInsertData = await gobyHomes();
+//     }
+//     // if (fromInsertData === true && fromInsertImage === true) {
+//     //   await concatePropertyImages();
+//     // }
+//     console.log("running a task every 30 minute.");
+//   } catch (error) {
+//     console.log("Something went wrong in 30 min cron.");
+//   }
+// });
 
-Cron("*/50 * * * *", async () => {
-  try {
-    const recordUpdateResult = await recordUpdate();
-    // if (recordUpdateResult.type === true) {
-    //   await updateBindPropertyImages(recordUpdateResult.data);
-    // }
-    console.log("running a task every 50 minute.");
-  } catch (error) {
-    console.log("Something went wrong in 50 min cron.");
+// Cron("*/50 * * * *", async () => {
+//   try {
+//     const recordUpdateResult = await recordUpdate();
+//     // if (recordUpdateResult.type === true) {
+//     //   await updateBindPropertyImages(recordUpdateResult.data);
+//     // }
+//     console.log("running a task every 50 minute.");
+//   } catch (error) {
+//     console.log("Something went wrong in 50 min cron.");
+//   }
+// });
+
+let running = false;
+
+nodecron.schedule("*/1 * * * *", () => {
+  if (running) {
+    console.log(
+      "Previous execution still in progress. Skipping this execution."
+    );
+    return;
   }
+  running = true;
+  console.log("Running cron job every two minutes");
+
+  // Simulate a long-running task
+  setTimeout(() => {
+    console.log("Long-running task complete");
+    running = false;
+  }, 120000); // 5 seconds
 });
