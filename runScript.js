c035bd4c377cf9305bdf04bddd335110c4e7c2c1
Tamojit <tamojit@sentientgeeks.com>
2023-05-05 17:15:12 +0530

    script code swap

--- runScript.js
+++ runScript.js
@@ -1,165 +1,136 @@
-const _ = require("lodash");
+const RETS = require("node-rets");
+const fs = require("fs");
 const MongoClient = require("mongodb").MongoClient;
 const CONSTANTS = require("./constants");
 
-const main = async (req, res) => {
-  MongoClient.connect(CONSTANTS.DB_CONNECTION_URI)
-    .then(async (client) => {
-      const connect = client.db(CONSTANTS.DB_NAME);
-      const collection = connect.collection("propertyData");
-      let count = 1;
-      await collection.find().forEach(async function (obj) {
-        // CODE TO GENERATE FULL ADDRESS
-        // const place1 = [];
-        // if (obj?.address?.street_number) {
-        //   place1.push(obj?.address?.street_number);
-        // }
-        // if (obj?.address?.street) {
-        //   place1.push(obj?.address?.street);
-        // }
-        // if (obj?.address?.street_suffix) {
-        //   place1.push(obj?.address?.street_suffix);
-        // }
-        // if (obj?.address?.street_dir_suffix) {
-        //   place1.push(obj?.address?.street_dir_suffix);
-        // }
-        // if (obj?.address?.street_dir_prefix) {
-        //   place1.push(obj?.address?.street_dir_prefix);
-        // }
-        // const addrPart1 = place1.join(" ");
-        // const addrArr = [];
-        // let zippart;
-        // if (obj?.address?.city) {
-        //   addrArr.push(obj?.address?.city);
-        // }
-        // if (obj?.other_data?.state) {
-        //   addrArr.push(obj?.other_data?.state);
-        // }
-        // if (obj?.other_data?.zipcode) {
-        //   zippart = obj?.other_data?.zipcode;
-        // }
-        // const addrPart2 = addrArr.join(", ");
-        // const twoPartAddr = addrPart1.concat(", ", addrPart2);
-        // const fullAddr = twoPartAddr.concat(" ", zippart);
-        // CODE TO GENERATE FULL ADDRESS
+const client = RETS.initialize({
+  loginUrl: "http://bright-rets.brightmls.com:6103/cornerstone/login",
+  username: "3348441",
+  password: "vUjeyasAmepri7eqehIPhifib",
+  version: "RETS/1.8",
+  userAgent: "Bright RETS Application/1.0",
+  logLevel: "info",
+});
 
-        // const fullBathrooms =
-        //   obj.bathrooms +
-        //   (obj.other_data.half_bathrooms
-        //     ? parseInt(obj?.other_data?.half_bathrooms)
-        //     : 0);
+async function checkExistingRecord(data) {
+  const client = new MongoClient(CONSTANTS.DB_CONNECTION_URI);
+  try {
+    await client.connect();
+    const collection = client
+      .db(CONSTANTS.DB_NAME)
+      .collection("propertyDataImages");
+    const ddt = await collection.find({ MediaURL: data.MediaURL }).toArray();
+    if (ddt[0]) {
+      return ddt[0];
+    } else {
+      return false;
+    }
+  } catch (e) {
+    console.error("error from checkExistingRecord imageUploadAfterInsert()", e);
+    return false;
+  }
+}
 
-        await collection.updateOne(
-          { _id: obj._id },
-          {
-            $set: {
-              // fullBathrooms: fullBathrooms,
-              // "address.fullAddress": fullAddr,
-              // "other_data.DOM": obj?.other_data?.DOM
-              //   ? parseInt(obj?.other_data?.DOM)
-              //   : 0,
-              // "other_data.HOA_Fee": obj?.other_data?.HOA_Fee
-              //   ? parseInt(obj?.other_data?.HOA_Fee)
-              //   : 0,
-              // bedrooms: obj?.bedrooms ? parseInt(obj.bedrooms) : 0,
-              // bathrooms: obj?.bathrooms ? parseInt(obj.bathrooms) : 0,
-              // TaxTotalFinishedSqFt: obj?.TaxTotalFinishedSqFt
-              //   ? parseInt(obj.TaxTotalFinishedSqFt)
-              //   : 0,
-              // listing_price: obj?.listing_price
-              //   ? parseFloat(obj.listing_price)
-              //   : 0,
-              // "other_data.HOA_Y/N": obj?.other_data?.HOA_Yu2f_N
-              //   ? obj?.other_data?.HOA_Yu2f_N
-              //   : "0",
-              // "other_data.Condo/Coop_Association_Y/N": obj?.other_data
-              //   ?.Condou2f_Coop_Association_Yu2f_N
-              //   ? obj?.other_data?.Condou2f_Coop_Association_Yu2f_N
-              //   : "0",
-              // "other_data.Condo/Coop_Fee": obj?.other_data?.Condou2f_Coop_Fee
-              //   ? obj?.other_data?.Condou2f_Coop_Fee
-              //   : "",
-              // "other_data.Condo/Coop_Fee_Frequency": obj?.other_data
-              //   ?.Condou2f_Coop_Fee_Frequency
-              //   ? obj?.other_data?.Condou2f_Coop_Fee_Frequency
-              //   : "",
-            },
-            $unset: {
-              //   __key__: "",
-              //   __error__: "",
-              //   __has_error__: "",
-              //   "other_data.__key__": "",
-              //   "address.__key__": "",
-              //   "image.__key__": "",
-              propertyDataImages: "",
-            },
+const imageUploadAfterInsert = async () => {
+  const listingChunks = [
+    "DCDC2093888",
+    "DCDC2094896",
+    "DCDC2094706",
+    "DCDC2094568",
+    "DCDC2093676",
+    "DCDC2094442",
+    "DCDC2092906",
+    "DCDC2093544",
+    "DCDC2094434",
+    "DCDC2094198",
+    "DCDC2094484",
+    "DCDC2093358",
+    "DCDC2093438",
+    "DCDC2094550",
+    "DCDC2093576",
+    "DCDC2093130",
+    "DCDC2092496",
+    "DCDC2094464",
+    "DCDC2094268",
+    "DCDC2092264",
+    "DCDC2093686",
+    "DCDC2092244",
+    "DCDC2093378",
+    "DCDC2094770",
+    "DCDC2094852",
+    "DCDC2094854",
+    "DCDC2093848",
+    "DCDC2081616",
+    "DCDC2093958",
+    "DCDC2094846",
+  ];
+  if (listingChunks) {
+    let records = [];
+
+    for (let j = 0; j < listingChunks.length; j++) {
+      const id = listingChunks[j];
+      if (id) {
+        try {
+          const query = await client.search(
+            "Media",
+            "PROP_MEDIA",
+            `(ListingId=${id})`,
+            {
+              Select:
+                "ListingId,MediaURL,MediaURLFull,MediaURLHD,MediaURLHiRes,MediaURLThumb,MediaURLMedium",
+            }
+          );
+          if (query.Objects && query.Objects.length > 0) {
+            for (const obj of query.Objects) {
+              const chkData = await checkExistingRecord(obj);
+              if (!chkData) {
+                records.push(obj);
+              }
+              //records.push(obj);
+            }
           }
-        );
-        console.log(`${count} => update done`);
-        count++;
-      });
-      console.log("update done");
-    })
-    .catch((err) => {
-      console.log(err.Message);
+        } catch (err) {
+          console.error(
+            `Error searching for ListingId ${id}: ${err.message} from imageUploadAfterInsert()`
+          );
+          continue; // Skip to next iteration of the loop
+        }
+      }
+    }
+    if (records.length > 0) {
+      await addRecordsToMongoDBImage(records);
+      console.log(
+        "All images fetched and added successfully! imageUploadAfterInsert()"
+      );
+    } else {
+      console.log("No images available to add! imageUploadAfterInsert()");
+    }
+    return true;
+  } else {
+    return true;
+  }
+};
+
+const addRecordsToMongoDBImage = async (records) => {
+  const client = new MongoClient(CONSTANTS.DB_CONNECTION_URI);
+  try {
+    await client.connect();
+    const collection = client
+      .db(CONSTANTS.DB_NAME)
+      .collection("propertyDataImages");
+    await collection.insertMany(records, (err, res) => {
+      if (err) throw err;
+      console.log(
+        `${res.insertedCount} documents inserted into propertyDataImages`
+      );
+      client.close();
     });
-};
-
-const removeDuplicateData = async () => {
-  const client = new MongoClient(CONSTANTS.DB_CONNECTION_URI);
-  const collection = client.db(CONSTANTS.DB_NAME).collection("propertyData");
-  let count = 1;
-  const gpdt = collection.aggregate([
-    { $group: { _id: "$listing_id", count: { $sum: 1 } } },
-    { $match: { count: { $gt: 1 } } },
-    { $unwind: "$_id" },
-    { $match: { _id: { $ne: null } } },
-  ]);
-  let cnt = 1;
-  let dupIds = [];
-
-  for (const doc of await gpdt.toArray()) {
-    const dup = await collection.find({ listing_id: doc._id }).skip(1);
-    for (const iterator of await dup.toArray()) {
-      dupIds.push(iterator._id);
-    }
-    console.log(`${cnt} data deleted ${doc._id} => ${doc.count}`);
-    cnt++;
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
   }
-  collection.deleteMany({ _id: { $in: dupIds } });
 };
 
-const removeDuplicateImage = async () => {
-  const client = new MongoClient(CONSTANTS.DB_CONNECTION_URI);
-  const collection = client
-    .db(CONSTANTS.DB_NAME)
-    .collection("propertyDataImages");
-  let count = 1;
-  const gpdt = collection.aggregate([
-    { $group: { _id: "$MediaURL", count: { $sum: 1 } } },
-    { $match: { count: { $gt: 1 } } },
-    { $unwind: "$_id" },
-    { $match: { _id: { $ne: null } } },
-  ]);
-  let cnt = 1;
-  let dupIds = [];
-
-  for (const doc of await gpdt.toArray()) {
-    const dup = await collection.find({ MediaURL: doc._id }).skip(1);
-    for (const iterator of await dup.toArray()) {
-      dupIds.push(iterator._id);
-    }
-    console.log(`${cnt} deleted ${doc.count}`);
-    if (cnt === 70000) break;
-    cnt++;
-  }
-  console.log("cnt>>>", cnt);
-  collection.deleteMany({ _id: { $in: dupIds } });
-  console.log("DELETE COMPLETED !!!");
-};
-
-//concatePropertyImages();
-// main();
-// removeDuplicateData();
-
-// removeDuplicateImage();
+imageUploadAfterInsert();
+// module.exports = imageUploadAfterInsert;
