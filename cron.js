4cc84f84c5f3e0717979b0684c088483c27fc7c7
Tamojit <tamojit@sentientgeeks.com>
2023-05-08 21:46:58 +0530

    cron time update

--- cron.js
+++ cron.js
@@ -1,4 +1,3 @@
-const nodeCorn = require("node-cron");
 const gobyHomes = require("./insertion");
 const recordUpdate = require("./updation");
 const imageUpload = require("./imageUpload");
@@ -6,45 +5,45 @@ const imageUploadAfterInsert = require("./imageUploadAfterInsert");
 const Cron = require("croner");
 const os = require("os");
 
-setInterval(() => {
-  const cpuUsage = os
-    .cpus()
-    .reduce(
-      (acc, cpu) =>
-        acc + cpu.times.user + cpu.times.nice + cpu.times.sys + cpu.times.irq,
-      0
-    );
-  const systemUptime = os.uptime();
-  const processUptime = process.uptime();
-  const cpuUsagePercent =
-    (100 * cpuUsage) /
-    ((systemUptime - processUptime) * os.cpus().length * 1000);
-  const used = process.memoryUsage().heapUsed / 1024 / 1024;
+// setInterval(() => {
+//   const cpuUsage = os
+//     .cpus()
+//     .reduce(
+//       (acc, cpu) =>
+//         acc + cpu.times.user + cpu.times.nice + cpu.times.sys + cpu.times.irq,
+//       0
+//     );
+//   const systemUptime = os.uptime();
+//   const processUptime = process.uptime();
+//   const cpuUsagePercent =
+//     (100 * cpuUsage) /
+//     ((systemUptime - processUptime) * os.cpus().length * 1000);
+//   const used = process.memoryUsage().heapUsed / 1024 / 1024;
 
-  console.log(
-    `The current memory usage is approximately ${used.toFixed(2)} MB`
-  );
-  console.log(`CPU Usage: ${cpuUsagePercent.toFixed(2)}%`);
-}, 1000);
+//   console.log(
+//     `The current memory usage is approximately ${used.toFixed(2)} MB`
+//   );
+//   console.log(`CPU Usage: ${cpuUsagePercent.toFixed(2)}%`);
+// }, 1000);
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 const cronJob1 = async () => {
   let fromInsertData = false;
   try {
-    console.log("running a task every 10 minute.", new Date().toISOString());
+    console.log("running a task every 30 minute.", new Date().toISOString());
     fromInsertData = await gobyHomes();
     if (fromInsertData && fromInsertData.length > 0) {
       await sleep(10000);
       await imageUploadAfterInsert(fromInsertData);
     }
   } catch (error) {
-    console.log("Something went wrong in 10 min Insert cron.");
+    console.log("Something went wrong in 30 min Insert cron.");
   }
 };
 
 let corn1Running = false;
 
-Cron("*/10 * * * *", async () => {
+Cron("*/30 * * * *", async () => {
   if (corn1Running) {
     return;
   }
@@ -60,7 +59,7 @@ Cron("*/10 * * * *", async () => {
 
 let corn2Running = false;
 
-Cron("*/25 * * * *", async () => {
+Cron("*/55 * * * *", async () => {
   let fromRecordUpdate = false;
   if (corn2Running) {
     return;
@@ -68,14 +67,14 @@ Cron("*/25 * * * *", async () => {
   corn2Running = true;
 
   try {
-    console.log("running a task every 25 minute.", new Date().toISOString());
+    console.log("running a task every 55 minute.", new Date().toISOString());
     fromRecordUpdate = await recordUpdate();
     if (fromRecordUpdate) {
       await sleep(10000);
       await imageUpload();
     }
   } catch (error) {
-    console.log("Something went wrong in 25 min Update cron.");
+    console.log("Something went wrong in 55 min Update cron.");
   } finally {
     corn2Running = false;
   }
