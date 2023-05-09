96808d628c2fd488ac0bd041063562b3330459d4
Tamojit <tamojit@sentientgeeks.com>
2023-05-09 19:44:26 +0530

    remove cpu uses interval

--- cron.js
+++ cron.js
@@ -5,26 +5,26 @@ const imageUploadAfterInsert = require("./imageUploadAfterInsert");
 const Cron = require("croner");
 const os = require("os");
 
-setInterval(() => {
-  const cpuUsage = os
-    .cpus()
-    .reduce(
-      (acc, cpu) =>
-        acc + cpu.times.user + cpu.times.nice + cpu.times.sys + cpu.times.irq,
-      0
-    );
-  const systemUptime = os.uptime();
-  const processUptime = process.uptime();
-  const cpuUsagePercent =
-    (100 * cpuUsage) /
-    ((systemUptime - processUptime) * os.cpus().length * 1000);
-  const used = process.memoryUsage().heapUsed / 1024 / 1024;
+// setInterval(() => {
+//   const cpuUsage = os
+//     .cpus()
+//     .reduce(
+//       (acc, cpu) =>
+//         acc + cpu.times.user + cpu.times.nice + cpu.times.sys + cpu.times.irq,
+//       0
+//     );
+//   const systemUptime = os.uptime();
+//   const processUptime = process.uptime();
+//   const cpuUsagePercent =
+//     (100 * cpuUsage) /
+//     ((systemUptime - processUptime) * os.cpus().length * 1000);
+//   const used = process.memoryUsage().heapUsed / 1024 / 1024;
 
-  console.log(
-    `The current memory usage is approximately ${used.toFixed(2)} MB`
-  );
-  console.log(`CPU Usage: ${cpuUsagePercent.toFixed(2)}%`);
-}, 1000);
+//   console.log(
+//     `The current memory usage is approximately ${used.toFixed(2)} MB`
+//   );
+//   console.log(`CPU Usage: ${cpuUsagePercent.toFixed(2)}%`);
+// }, 1000);
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 const cronJob1 = async () => {
