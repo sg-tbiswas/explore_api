86efc08795e8c6198c8da06572d372cb2183530e
Tamojit <tamojit@sentientgeeks.com>
2023-05-22 13:40:56 +0530

    shell script changes restart

--- cron.js
+++ cron.js
@@ -83,7 +83,7 @@ let corn2Running = false;
 
 //Cron("0 0 * * *", { timezone: "Asia/Kolkata" }, async () => {
 Cron("*/1 * * * *", async () => {
-  exec("sudo systemctl start mongod.service", (error, stdout, stderr) => {
+  exec("sudo systemctl restart mongod.service", (error, stdout, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
       return;
