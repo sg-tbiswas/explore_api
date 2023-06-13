1176c101a3e53377dbd58d8d396b55006bb85f9a
Tamojit <tamojit@sentientgeeks.com>
2023-06-13 21:27:38 +0530

    show actual time

--- statusUpdate.js
+++ statusUpdate.js
@@ -28,6 +28,38 @@ const statusUpdate = async () => {
     console.log(formattedTime, currentDate);
     console.log("TimeZone", Intl.DateTimeFormat().resolvedOptions().timeZone);
 
+    let date_ob = new Date();
+    // current date
+    // adjust 0 before single digit date
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    // current year
+    let year = date_ob.getFullYear();
+    // current hours
+    let hours = date_ob.getHours();
+    // current minutes
+    let minutes = date_ob.getMinutes();
+    // current seconds
+    let seconds = date_ob.getSeconds();
+
+    // prints date & time in YYYY-MM-DD HH:MM:SS format
+    console.log(
+      year +
+        "-" +
+        month +
+        "-" +
+        date +
+        " " +
+        hours +
+        ":" +
+        minutes +
+        ":" +
+        seconds
+    );
+    // prints time in HH:MM format
+    console.log(hours + ":" + minutes);
+
     const newFormattedTime = new Date().toISOString().slice(0, -1);
     const temp = await RETS_CLIENT.search(
       "Property",
