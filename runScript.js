6a38679691793bfa883aaf659d2b8cb9f226370f
Tamojit <tamojit@sentientgeeks.com>
2023-06-21 16:54:42 +0530

    data limit 10k runscript

--- runScript.js
+++ runScript.js
@@ -45,7 +45,7 @@ const statusUpdate = async () => {
       "Property",
       "ALL",
       `~(StandardStatus=|Active,Pending,Active Under Contract) AND (ModificationTimestamp=${formattedTime}+)`,
-      { limit: 50000, offset, Select: feildsValues.join(",") }
+      { limit: 10000, offset, Select: feildsValues.join(",") }
     );
 
     count = parseInt(temp.TotalCount);
