f24d1e9bf47d8e86ab3a9e933a9d215d4fd5fe41
Tamojit <tamojit@sentientgeeks.com>
2023-06-21 20:45:42 +0530

    data limit changed

--- runScript.js
+++ runScript.js
@@ -45,7 +45,7 @@ const statusUpdate = async () => {
       "Property",
       "ALL",
       `~(StandardStatus=|Active,Pending,Active Under Contract) AND (ModificationTimestamp=${formattedTime}+)`,
-      { limit: 50, offset, Select: feildsValues.join(",") }
+      { limit: 2500, offset, Select: feildsValues.join(",") }
     );
 
     count = parseInt(temp.TotalCount);
