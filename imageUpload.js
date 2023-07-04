d285b160dc01f254d9babf074b7475959919ab25
Tamojit <tamojit@sentientgeeks.com>
2023-07-04 21:45:46 +0530

    query error fix

--- imageUpload.js
+++ imageUpload.js
@@ -115,7 +115,7 @@ const getListingIds = async () => {
     const listingIdData = await RETS_CLIENT.search(
       "Property",
       "ALL",
-      `(StandardStatus=|Active,Pending,Active Under Contract) AND (ModificationTimestamp=${newFormattedTime}+})`,
+      `(StandardStatus=|Active,Pending,Active Under Contract) AND (ModificationTimestamp=${newFormattedTime}+)`,
       {
         Select: "ListingId",
       }
