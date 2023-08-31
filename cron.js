0ca6e6429448ab4d2d79c3adebfb92f23cf5d521
Tamojit <tamojit@sentientgeeks.com>
2023-08-31 15:44:41 +0530

    code update

--- cron.js
+++ cron.js
@@ -4,7 +4,7 @@ const imageUpload = require("./imageUpload");
 const statusUpdate = require("./statusUpdate");
 const imageUploadAfterInsert = require("./imageUploadAfterInsert");
 const Cron = require("croner");
-const dbConnection = require('./dbConnection');
+const dbConnection = require("./dbConnection");
 const os = require("os");
 const { exec } = require("child_process");
 
@@ -21,60 +21,70 @@ Cron("*/30 * * * *", async () => {
     console.warn("Already running 30 minute cron.", new Date().toUTCString());
     return;
   }
-
   corn1Running = true;
   try {
-    console.log("running a task every 30 minute.", new Date().toUTCString());
     const db = new dbConnection();
     const client = await db.connect();
+    try {
+      console.log("running a task every 30 minute.", new Date().toUTCString());
 
-    fromInsertData = await gobyHomes(client);
-    if (fromInsertData && fromInsertData.length > 0) {
-      await sleep(10000);
-      await imageUploadAfterInsert(fromInsertData, client);
+      fromInsertData = await gobyHomes(client);
+      if (fromInsertData && fromInsertData.length > 0) {
+        await sleep(10000);
+        await imageUploadAfterInsert(fromInsertData, client);
+        corn1Running = false;
+      }
+      await db.disconnect();
+    } catch (error) {
+      console.error(
+        `Something went wrong in 30 min Insert cron.${new Date().toUTCString()}`,
+        error.message
+      );
       corn1Running = false;
+    } finally {
+      await db.disconnect();
     }
-    await client.close();
   } catch (error) {
-    console.error(
-      `Something went wrong in 30 min Insert cron.${new Date().toUTCString()}`,
-      error.message
-    );
-    corn1Running = false;
-  }finally{
-    const db = new dbConnection();
-    await db.disconnect();
+    console.log("DB connection error!", error);
   }
 });
 
 Cron("45 * * * *", async () => {
   let fromRecordUpdate = false;
   if (corn2Running) {
-    console.warn("Already running 45 minute of hour cron.", new Date().toUTCString());
+    console.warn(
+      "Already running 45 minute of hour cron.",
+      new Date().toUTCString()
+    );
     return;
   }
   corn2Running = true;
-
   try {
     const db = new dbConnection();
     const client = await db.connect();
-
-    console.log("running a task every 45 minute of hour.", new Date().toUTCString());
-    fromRecordUpdate = await recordUpdate(client);
-    if (fromRecordUpdate) {
-      await sleep(10000);
-      await imageUpload(client);
+    try {
+      console.log(
+        "running a task every 45 minute of hour.",
+        new Date().toUTCString()
+      );
+      fromRecordUpdate = await recordUpdate(client);
+      if (fromRecordUpdate) {
+        await sleep(10000);
+        await imageUpload(client);
+        corn2Running = false;
+      }
+      await db.disconnect();
+    } catch (error) {
+      console.error(
+        `Something went wrong in 45 min of hour Update cron.${new Date().toUTCString()}`,
+        error.message
+      );
       corn2Running = false;
+    } finally {
+      await db.disconnect();
     }
   } catch (error) {
-    console.error(
-      `Something went wrong in 45 min of hour Update cron.${new Date().toUTCString()}`,
-      error.message
-    );
-    corn2Running = false;
-  }finally{
-    const db = new dbConnection();
-    await db.disconnect();
+    console.log("DB connection error!", error);
   }
 });
 
@@ -88,18 +98,22 @@ Cron("*/20 * * * *", async () => {
   try {
     const db = new dbConnection();
     const client = await db.connect();
-    console.log("running a task every 20 min.", new Date().toUTCString());
-    await statusUpdate(client);
-    corn3Running = false;
+    try {
+      console.log("running a task every 20 min.", new Date().toUTCString());
+      await statusUpdate(client);
+      corn3Running = false;
+      await db.disconnect();
+    } catch (error) {
+      console.error(
+        `Something went wrong in 20 min status Update cron.${new Date().toUTCString()}`,
+        error.message
+      );
+      corn3Running = false;
+    } finally {
+      await db.disconnect();
+    }
   } catch (error) {
-    console.error(
-      `Something went wrong in 20 min status Update cron.${new Date().toUTCString()}`,
-      error.message
-    );
-    corn3Running = false;
-  }finally{
-    const db = new dbConnection();
-    await db.disconnect();
+    console.log("DB connection error!", error);
   }
 });
 
--- dbConnection.js
+++ dbConnection.js
@@ -2,27 +2,27 @@ const MongoClient = require("mongodb").MongoClient;
 const CONSTANTS = require("./constants");
 
 class Database {
-    constructor() {
-      this.client = null;
-    }
-  
-    async connect() {
-      if (!this.client) {
-        this.client = new MongoClient(CONSTANTS.DB_CONNECTION_URI, {
-          maxPoolSize: 2,
-          maxConnecting: 2,
-        });
-        await this.client.connect();
-      }
-      return this.client;
-    }
-  
-    async disconnect() {
-      if (this.client) {
-        await this.client.close();
-        this.client = null;
-      }
-    }
+  constructor() {
+    this.client = null;
   }
 
-  module.exports = Database;
+  async connect() {
+    if (!this.client) {
+      this.client = new MongoClient(CONSTANTS.DB_CONNECTION_URI, {
+        maxPoolSize: 2,
+        maxConnecting: 2,
+      });
+      await this.client.connect();
+    }
+    return this.client;
+  }
+
+  async disconnect() {
+    if (this.client) {
+      await this.client.close();
+      this.client = null;
+    }
+  }
+}
+
+module.exports = Database;
