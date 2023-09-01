72ed74fda4d1c36a9243dbce3d9fabc00daf84e5
Tamojit <tamojit@sentientgeeks.com>
2023-09-01 19:32:12 +0530

    update data for pending

--- scripts/updateDataScript.js
+++ scripts/updateDataScript.js
@@ -35,7 +35,7 @@ const recordUpdate = async () => {
       const temp = await RETS_CLIENT.search(
         "Property",
         "ALL",
-        `(StandardStatus=|Active) AND (MLSListDate=2023-08-01-2023-08-31)`,
+        `(StandardStatus=|Pending) AND (MLSListDate=2023-08-01-2023-08-31)`,
         {
           Select: feildsValues.join(","),
         }
