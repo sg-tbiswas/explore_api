5de9d7eb51620d7e00af73f563e5065b0e402e8c
Tamojit <tamojit@sentientgeeks.com>
2023-09-06 16:54:38 +0530

    updateScript date range change

--- scripts/updateDataScript.js
+++ scripts/updateDataScript.js
@@ -36,7 +36,7 @@ const recordUpdate = async () => {
       const temp = await RETS_CLIENT.search(
         "Property",
         "ALL",
-        `(StandardStatus=|Active Under Contract) AND (MLSListDate=2023-07-01-2023-07-31)`,
+        `(StandardStatus=|Active) AND (MLSListDate=2023-06-01-2023-06-30)`,
         {
           Select: feildsValues.join(","),
         }
