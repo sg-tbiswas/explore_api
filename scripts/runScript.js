8dfe0a276294ba747dc374766473e0dd6ec3923b
Tamojit <tamojit@sentientgeeks.com>
2023-09-23 20:16:41 +0530

    runscript date 1809

--- scripts/runScript.js
+++ scripts/runScript.js
@@ -65,7 +65,7 @@ const fetchRecords = async (resource, className, keyMapping, client) => {
     const records = await RETS_CLIENT.search(
       resource,
       className,
-      `(StandardStatus=|Active,Pending,Active Under Contract)  AND (MLSListDate=2023-09-11)`,
+      `(StandardStatus=|Active,Pending,Active Under Contract)  AND (MLSListDate=2023-09-18)`,
       {
         Select: feildsValues.join(","),
       }
