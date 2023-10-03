51b638787335bee337c5de70f66ab19536f55639
Tamojit <tamojit@sentientgeeks.com>
2023-10-03 09:44:08 +0530

    runScript date change 2909

--- scripts/runScript.js
+++ scripts/runScript.js
@@ -65,7 +65,7 @@ const fetchRecords = async (resource, className, keyMapping, client) => {
     const records = await RETS_CLIENT.search(
       resource,
       className,
-      `(StandardStatus=|Active,Pending,Active Under Contract)  AND (MLSListDate=2023-09-24+)`,
+      `(StandardStatus=|Active,Pending,Active Under Contract)  AND (MLSListDate=2023-09-29+)`,
       {
         Select: feildsValues.join(","),
       }
